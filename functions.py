78325a2e5f7c1952505a9526cd9e22eacaa074d4
Andoni <aagirre92@gmail.com>
2023-01-20 13:42:34 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,46 @@
+import requests
+
+
+def get_token_password(cr_url, username, password):
+    endpoint = "/v1/authentication"
+    url = cr_url + endpoint
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "username": username,
+        "password": password
+    }
+
+    r = requests.post(url=url, headers=headers, json=payload, verify=False)
+    r.raise_for_status()
+    return r.json()['token']
+
+
+def create_gv(cr_url, token, global_value):
+    endpoint = "/v1/globalvalues/values"
+    url = cr_url + endpoint
+    headers = {
+        "Content-Type": "application/json",
+        "X-Authorization": token
+    }
+
+    payload = {
+        "key": global_value["name"],
+        "name": global_value["name"],
+        "description": global_value["description"],
+        "type": global_value["type"],
+        "isOverridable": str(global_value["isOverridable"]),
+        "isPublic": True,
+        "value": {
+            "type": global_value["type"],
+            str(global_value["type"]).lower(): global_value["value"]
+        },
+        "userId": "0"
+    }
+    # print(payload)
+    r = requests.put(url=url, headers=headers, json=payload,
+                     verify=False)  # POST method fails if gv has been already created, use PUT
+    r.raise_for_status()
+    return r.json()
---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import argparse
+import functions as f
+
+if __name__ == '__main__':
+    # Get arguments from CMD
+    parser = argparse.ArgumentParser(prog="A360 - Global Value importer",
+                                     description='Imports Global Values to A360 CR given a Global Value csv template')
+    parser.add_argument("--cr_url", help="url of the control room (without the / at the end!) ")
+    parser.add_argument("--username", help="full username with domain if AD")
+    parser.add_argument("--password", help="password")
+    parser.add_argument("--gv_csv_path", help="global value csv file path")
+
+    args = parser.parse_args()
+
+    cr_url = str(args.cr_url)
+    user = str(args.username)
+    password = str(args.password)
+    gv_file_path = str(args.gv_csv_path)
+
+    # Read csv
+    df = pd.read_csv(gv_file_path)
+
+    # Substitute NaN by ''
+    df = df.fillna('')
+    # Get token
+    token = f.get_token_password(cr_url, user, password)
+
+    # Create gv (or overwrite existing) loop:
+    for index, global_value in df.iterrows():
+        f.create_gv(cr_url, token, global_value)
+
+    print("Global Values imported successfully")
