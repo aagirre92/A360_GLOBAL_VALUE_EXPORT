1892a9f7febbf81d9cfb51dabb36bf3fea5762cd
Andoni <aagirre92@gmail.com>
2023-01-26 17:06:51 +0100

    api key authentication

--- functions.py
+++ functions.py
@@ -18,6 +18,23 @@ def get_token_password(cr_url, username, password):
     return r.json()['token']
 
 
+def get_token_apikey(cr_url, username, api_key):
+    endpoint = "/v1/authentication"
+    url = cr_url + endpoint
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "username": username,
+        "apiKey": api_key
+    }
+
+    r = requests.post(url=url, headers=headers, json=payload, verify=False)
+    r.raise_for_status()
+    return r.json()['token']
+
+
 def create_gv(cr_url, token, global_value):
     endpoint = "/v1/globalvalues/values"
     url = cr_url + endpoint
--- main.py
+++ main.py
@@ -8,14 +8,16 @@ if __name__ == '__main__':
                                      description='Imports Global Values to A360 CR given a Global Value csv template')
     parser.add_argument("--cr_url", help="url of the control room (without the / at the end!) ")
     parser.add_argument("--username", help="full username with domain if AD")
-    parser.add_argument("--password", help="password")
+    parser.add_argument("--api_key", help="API key for user")
+    # parser.add_argument("--password", help="password")
     parser.add_argument("--gv_csv_path", help="global value csv file path")
 
     args = parser.parse_args()
 
     cr_url = str(args.cr_url)
     user = str(args.username)
-    password = str(args.password)
+    # password = str(args.password)
+    api_key = str(args.api_key)
     gv_file_path = str(args.gv_csv_path)
 
     # Read csv
@@ -24,7 +26,7 @@ if __name__ == '__main__':
     # Substitute NaN by ''
     df = df.fillna('')
     # Get token
-    token = f.get_token_password(cr_url, user, password)
+    token = f.get_token_apikey(cr_url, user, api_key)
 
     # Create gv (or overwrite existing) loop:
     for index, global_value in df.iterrows():
